0ac9bacd1cf2c87356492a337150a3467e8db83b
Arkadi Chubrik <arkadi@chubrik.org>
2026-04-18 10:57:36 +0300

    Fix tests

--- build/build-test.ts
+++ build/build-test.ts
@@ -1,11 +1,11 @@
-import { cpuPipeline, DIST_DIR, SRC_DIR, writeToPath } from './utils.ts';
+import { rmSync } from 'fs';
+import { cpuPipeline, DIST_DIR, SRC_DIR } from './utils.ts';
 
 export async function setup() {
-  const path = `${SRC_DIR}/z80-test-hook.ts`;
-  const { built, processed } = await cpuPipeline(path, { test: true });
+  rmSync(`${DIST_DIR}/temp/z80-test`, { recursive: true, force: true });
 
-  const fileName = 'z80-test';
-  writeToPath(`${DIST_DIR}/temp/${fileName}/${fileName}.js`, processed);
+  const path = `${SRC_DIR}/z80-test.ts`;
+  const { built, processed } = await cpuPipeline(path, { test: true });
 
   console.log(`${path} (test): ${built.length} bytes → minified: ${processed.length} bytes`);
 }
